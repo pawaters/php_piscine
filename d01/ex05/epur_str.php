ab893d92fd49808d0b63a3722f4be659bcbe694e
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-12 12:17:45 +0300

    d01ex00-05 ready

---
+++ d01/ex05/epur_str.php
@@ -0,0 +1,12 @@
+#!/usr/local/bin/php
+<?php
+
+    if ($argc == 2)
+    {
+        $str = trim($argv[1]);
+        while ($str != str_replace('  ', ' ', $str))
+            $str = str_replace('  ', ' ', $str);
+    }
+    echo "$str\n";
+
+?>
