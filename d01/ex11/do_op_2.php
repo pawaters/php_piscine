ad78d897e2a2131de874ac5f31395ed3e3249a29
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-12 18:14:46 +0300

    d01ex11 - test and correct extra newline

--- d01/ex11/do_op_2.php
+++ d01/ex11/do_op_2.php
@@ -1,6 +1,40 @@
 #!/usr/local/bin/php
+
 <?php
+if ($argc == 2)
+{
+    $input = trim(preg_replace('/ +/', "", $argv[1]));
+    if (!preg_match('/^[0-9]+[%*+\-\/][0-9]+$/', $input))
+    {
+        exit("Syntax Error\n");
+    }
 
+    $op = strpbrk($input, "+-*/%");
+    $nb = preg_split('/[%*+\-\/]/', $input);
+ 
 
+    switch ($op[0])
+        {
+            case "+":
+                echo $nb[0] + $nb[1] . "\n";
+                break;
+            case "-":
+                echo $nb[0] - $nb[1] . "\n";
+                break;
+            case "*":
+                echo $nb[0] * $nb[1] . "\n";
+                break;
+            case "/":
+                echo $nb[0] / $nb[1] . "\n";
+                break;
+            case "%":
+                echo $nb[0] % $nb[1] . "\n";
+                break;
+            default:
+                echo "Incorrect Parameters\n";
+        }
+}
+else
+    echo "Incorrect Parameters\n";
 
 ?>
