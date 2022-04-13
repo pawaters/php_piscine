63e05b8b80b1507c8a9f5a267fd2eaa07d9c9d95
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-13 14:14:03 +0300

    d02ex00-02 ready

---
+++ d02/ex02/magnifying_glass.php
@@ -0,0 +1,20 @@
+#!/usr/local/bin/php
+<?php
+
+    function ft_upcase($matches)
+    {
+        return (str_replace($matches[1], strtoupper($matches[1]), $matches[0]));
+    }
+
+    if ($argc == 2)
+    {
+        $fh = fopen($argv[1], 'r');
+        $page = "";
+        while ($fh && !feof($fh))
+            $page .= fgets($fh);
+    $page = preg_replace_callback('/<a.*?title="(.*?)">/', "ft_upcase", $page);
+    $page = preg_replace_callback('/<a.*?>(.*?)</', "ft_upcase", $page);
+    echo $page;
+    }
+
+?>
