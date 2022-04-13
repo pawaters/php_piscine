3be2e59f13ac0362dc12b9cb7617f4a8bcab3f6f
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-13 17:47:08 +0300

    blocked on d02ex05

--- d02/ex05/denial.php
+++ d02/ex05/denial.php
@@ -3,6 +3,27 @@
 
 $fh = fopen($argv[1], 'r');
 $line = fgetcsv($fh, 0, ';');
-echo $line;
+$vars = $line;
+$key = $argv[2];
+
+$varsList = [];
+foreach ($line as $var)
+    $varsList[$var] = [];
+
+while ($line = fgetcsv($fh, 0, ';'))
+{
+    foreach ($line as $index => $value)
+    {
+        $varsList[$vars[$index]][$line[array_search($key, $vars)]] = $value;
+    }
+}
+extract($varsList);
+
+echo "Enter you command: ";
+while ($line = fgets(STDIN))
+{
+    echo "Enter your command: ;";
+}
+echo "\n";
 
 ?>
