af2525989f7bcccd172f69b6a1ee44fb0b408fc7
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-13 10:01:39 +0300

    d02ex00

--- README.md
+++
@@ -1,1 +0,0 @@
-My work (in progress) for the April '22 PHP Piscine at HIVE. 
---
+++ d02/ex00/another_world.php
@@ -0,0 +1,10 @@
+#!/usr/local/bin/php
+<?php
+    if ($argc > 1)
+    {
+        $str = trim($argv[1]);
+        $str = preg_replace('/\s+/', ' ', $str);
+        if ($str)
+            echo "$str" . "\n";
+    }
+?>
