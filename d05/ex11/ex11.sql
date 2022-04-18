f072c61fc41f377e3d595f308964042ff857ca9e
pawaters <86101754+pawaters@users.noreply.github.com>
2022-04-18 14:28:06 +0300

    d05ex00-11

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -1,8 +1,9 @@
-SELECT UPPER(user.last_name) AS `NAME`,
-	user.first_name,
-	subs.price FROM member
-	INNER JOIN user_card AS user ON member.id_user_card = user.id_user
-	INNER JOIN subscription AS subs ON subs.id_sub = member.id_sub
-	WHERE subs.price > 42
-	ORDER BY user.last_name, user.first_name ASC
+SELECT UPPER(user_card.last_name) AS `NAME`, 
+	user_card.first_name,
+    subscription.price 
+    FROM member
+    INNER JOIN user_card ON member.id_user_card = user_card.id_user
+    INNER JOIN subscription ON subscription.id_sub = member.id_sub
+    WHERE subscription.price > 42
+    ORDER BY user_card.last_name, user_card.first_name ASC
 ;
